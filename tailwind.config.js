3ba70f158a7c71d54fdaac700b7c3165df20e93e
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-06-13 21:47:34 +0800

    fix: tailwind intellisense

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+  content: ['content/**/*.{md,yml,json,json5,csv}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        // primary: colors.zinc,
+        // secondary: colors.teal,
+        // accent: colors.green,
+        // info: colors.yellow,
+      },
+    },
+  },
+}
