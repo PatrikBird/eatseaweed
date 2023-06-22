4fb76fde8b94b8296c36c3b043c0c42405dbd053
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-06-22 16:22:51 +0800

    chore: use motion to show navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -91,7 +91,7 @@ const flagIcon = computed(() => languageMapping[currentLanguage.value as Languag
       </div>
     </div>
 
-    <DisclosurePanel class="sm:hidden">
+    <DisclosurePanel v-motion-slide-top class="sm:hidden">
       <div class="space-y-1 pb-3 pt-2">
         <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
         <DisclosureButton as="a" href="#" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">
